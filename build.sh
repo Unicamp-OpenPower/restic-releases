4ac6d240212f0f321f7b7c89809ad23d6cd7c1fb
Luciano Zago <lcnzg7@gmail.com>
2019-10-23 14:11:23 -0300

    Update build

--- build.sh
+++ build.sh
@@ -2,11 +2,20 @@
 
 set -e
 
+echo "1"
 wget -q https://github.com/restic/restic/releases/download/v$RELEASE_VERSION/restic-$RELEASE_VERSION.tar.gz
+echo "2"
 tar -xzf restic-$RELEASE_VERSION.tar.gz
+echo "3"
 mv restic-$RELEASE_VERSION restic
+echo "4"
 cd restic
+echo "5"
 make all
+echo "6"
 mkdir output
+echo "7"
 mv restic output/restic-$RELEASE_VERSION
+echo "8"
 ls -la output
+echo "9"
